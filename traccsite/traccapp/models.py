fb40a30c62620b5366d5def9e7bfd6317b105840
Scott3-0 <bremmerscottw@gmail.com>
2021-07-07 09:35:14 -0700

    messed with DB

--- traccsite/traccapp/models.py
+++ traccsite/traccapp/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    user_id = models.IntegerField()
-    username = models.CharField(max_length=25)
-    password = models.CharField(max_length=50)
+class WeatherReport(models.Model):
+    sky_condition_choices = [
+        ('SKC', 'Clear skies'),
+        ('FEW', 'Few clouds'),
+        ('SCT', 'Scattered clouds'),
+        ('BKN', 'Broken'),
+        ('OVC', 'Overcast')
+    ]
+    wind_dir_choices = [
+        ('NAN', 'No wind'),
+        ('000', 'North'),
+        ('045', 'Northeast'),
+        ('090', 'East'),
+        ('135', 'Southeast'),
+        ('180', 'South'),
+        ('225', 'Southwest'),
+        ('270', 'West'),
+        ('315', 'Northwest')
+    ]
 
-class SavedLoc(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    lat = models.IntegerField()
-    lon = models.IntegerField()
+    location = models.CharField()
+    date = models.DateField()
+    sky_condition = models.CharField(max_length=2, choices=sky_condition_choices)
+    wind_dir = models.CharField(max_length=2, choices=wind_dir_choices)
+    wind_vel = models.FloatField(default=0)
+    temp = models.FloatField()
